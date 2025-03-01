695ffea2317e2f6254b9e017c514b782a94f6873
Marta <martastepaniuk52@gmail.com>
2025-03-01 21:26:54 +0200

    Test

--- main.tf
+++ main.tf
@@ -2,21 +2,33 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-data "aws_security_group" "allow_http_ssh" {
-  filter {
-    name   = "group-name"
-    values = ["allow_http_ssh"]
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "my_instance" {
   ami           = "ami-09a9858973b288bdd"
-  instance_type = "t3.micro"
+  instance_type = "t2.nano"
   key_name      = "keyforlab1"
 
-  vpc_security_group_ids = [data.aws_security_group.allow_http_ssh.id]
+  vpc_security_group_ids = ["sg-0f80e74fb54f67309"]
+
+  user_data = <<-EOF
+    #! /bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apache2
+    sudo systemctl start apache2
+    sudo systemctl enable apache2
+    echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+  EOF
 
   tags = {
-    Name = "Terraform-Managed-Instance"
+    Name = "my-instance"
   }
+}
+
+output "instance_ip" {
+  value = aws_instance.my_instance.public_ip
 }
